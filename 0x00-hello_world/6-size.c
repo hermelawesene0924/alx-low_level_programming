1935f99db1e3f0ecc72eb60a4f59dc3e62e101c2
hermelawesene0924 <yabsirawesene920@gmail.com>
2021-06-17 13:55:40 -0700

    hufff commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,8 +14,7 @@ long long int llInt;
 printf("Size of a char: %zu byte(s)\n", sizeof(charType));
 printf("Size of an int: %zu byte(s)\n", sizeof(intType));
 printf("Size of a long int: %zu byte(s)\n", sizeof(longInt));
-printf("Size of a lon long int: %zu byte(s)\n", sizeof(llInt));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(llInt));
 printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 return (0);
 }
-
