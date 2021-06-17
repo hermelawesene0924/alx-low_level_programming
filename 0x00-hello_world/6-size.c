fef81471b7d17d7732d10db665a7db586e71cf49
hermelawesene0924 <yabsirawesene920@gmail.com>
2021-06-17 12:04:57 -0700

    size commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+printf("Size of a long int: %zu byte(s)\n", sizeof(longInt));
+printf("Size of a lon long int: %zu byte(s)\n", sizeof(llInt));
+printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+return (0);
+}
