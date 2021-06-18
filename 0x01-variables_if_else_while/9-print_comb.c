cd85b8d07a7714b1d0a9c49de62afeebb61409a4
hermelawesene0924 <yabsirawesene920@gmail.com>
2021-06-18 11:28:21 -0700

    her commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,13 +7,12 @@
 int main(void)
 {
 int i;
-for ( i = '0'; i <= '9'; i++)
+for (i = '0'; i <= '9'; i++)
 {
 putchar(i);
 if (i != '9')
 putchar(',');
 putchar(' ');
 }
-putchar('\n');
 return (0);
 }
