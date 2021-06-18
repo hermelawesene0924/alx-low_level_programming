50c25606634f82a8a1d286cd060ad52420242dc4
hermelawesene0924 <yabsirawesene920@gmail.com>
2021-06-18 03:43:34 -0700

    you commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,20 +6,18 @@
  */
 int main(void)
 {
-int n = 0;
-printf("enter number:");
-scanf("%d", &n);
-if(n>0)
+int n;
+if (n > 0)
 {
-printf("%d is positive\n",n);
+printf("%d is positive\n", n);
 }
-else if(n<0)
+else if (n < 0)
 {
-printf("%d is negative\n",n);
+printf("%d is negative\n", n);
 }
 else
 {
-printf("%d is zero\n",n);
+printf("%d is zero\n", n);
 }
 return (0);
 }
