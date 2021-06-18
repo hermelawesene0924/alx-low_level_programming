d0c7e94bd6b981fcdedf9f5879021aaadd0802f9
hermelawesene0924 <yabsirawesene920@gmail.com>
2021-06-18 03:11:22 -0700

    if else commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,20 +6,20 @@
  */
 int main(void)
 {
-  int n = 0;
-  printf("enter number:");
-  scanf("%d", &n);
-  if(n>0)
-    {
-      printf("%d is positive\n",n);
-    }
-  else if(n<0)
-    {
-      printf("%d is negative\n",n);
-    }
-  else
-    {
-      printf("%d is negative\n",n);
-    }
-  return (0);
+int n = 0;
+printf("enter number:");
+scanf("%d", &n);
+if(n>0)
+{
+printf("%d is positive\n",n);
+}
+else if(n<0)
+{
+printf("%d is negative\n",n);
+}
+else
+{
+printf("%d is zero\n",n);
+}
+return (0);
 }
