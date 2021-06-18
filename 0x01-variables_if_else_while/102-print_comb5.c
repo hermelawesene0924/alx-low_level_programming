4c466b7e3599c05d41fbca55567981d6c03d812e
hermelawesene0924 <yabsirawesene920@gmail.com>
2021-06-18 13:09:52 -0700

    all variables commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 int c, i, k, j;
-for (c = 48; i <= 57; c++)
+for (c = 48; c <= 57; c++)
 {
 for (i = 48; i <= 57; i++)
 {
@@ -15,7 +15,7 @@ for (k = 48; k <= 57; k++)
 {
 for (j = 48; j <= 57; j++)
 {
-if (((k + j) > (c + i) && k >=c) || c < k)
+if (((k + j) > (c + i) && k >= c) || c < k)
 {
 putchar(c);
 putchar(i);
