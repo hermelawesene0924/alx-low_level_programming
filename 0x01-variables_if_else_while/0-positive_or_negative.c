fe20f6d3aadc02e0e9cecb7769371fd542429bb1
hermelawesene0924 <yabsirawesene920@gmail.com>
2021-06-18 02:37:40 -0700

    if commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  int n;
+  if(n>0)
+    {
+      printf("n is positive\n");
+    }
+  else if(n<0)
+    {
+      printf("n is negative\n");
+    }
+  else
+    {
+      printf("n is negative\n");
+    }
+  return (0);
+}
