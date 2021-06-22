2c7c14e72e81d40fc5138cf350dde3d34ccfac1c
hermelawesene0924 <yabsirawesene920@gmail.com>
2021-06-22 05:43:48 -0700

    j commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * main - The entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+print_alphabet();
+int i;
+for (i = 97; i < 123; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+return (0);
+}
