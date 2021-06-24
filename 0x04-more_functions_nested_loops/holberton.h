58c0d8428e20753059ed85110a1a7a7922ef25e5
hermelawesene0924 <yabsirawesene920@gmail.com>
2021-06-24 04:44:25 -0700

    mode commit

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,13 +1,25 @@
-int _isupper(int c);
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+extern int _putchar(char c);
 
-int _isdigit(int c);
+extern int _isupper(int c);
 
-int mul(int a, int b);
+extern int _isdigit(int c);
 
-void print_numbers(void);
+extern int mul(int a, int b);
 
-void print_most_numbers(void);
+extern void print_numbers(void);
 
-void more_numbers(void);
+extern void print_most_numbers(void);
 
-void print_line(int n);
+extern void more_numbers(void);
+
+extern void print_line(int n);
+
+extern void print_diagonal(int n);
+
+extern void print_square(int size);
+
+extern void print_triangle(int size);
+
+#endif
